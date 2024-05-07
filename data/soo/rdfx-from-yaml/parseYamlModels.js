932459619f3174a9b222d2f94b63e42dc4895e9e
Florent <fa+gitlab@mindmatcher.org>
2024-05-07 18:17:04 +0200

    feat: add soo implementation context + rename for good file name

--- data/soo/rdfx-from-yaml/parseYamlModels.js
+++ data/soo/rdfx-from-yaml/parseYamlModels.js
@@ -1,10 +1,14 @@
 import updateGraphMapFromYaml from './updateGraphMapFromYaml.js'
 
-export default function parseYamlModels({context, worldGraph}, ...ymlContents){
+export default function parseYamlModels({ context, worldGraph }, ...ymlContents) {
   const graphMap = new Map()
 
-  for(const model of ymlContents.flat()){
+  for (const model of ymlContents.flat()) {
     updateGraphMapFromYaml(graphMap, model, worldGraph)
+    // Parse the NS entries if they exist, add them to the context 
+    if (model.ns) {
+      Object.assign(context, model.ns)
+    }
   }
 
   const ld = {
