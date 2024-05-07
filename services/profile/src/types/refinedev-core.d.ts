c7d80fa7a90cbd90f12af154797c456eb9d6a779
Florent <fa+gitlab@mindmatcher.org>
2024-05-07 18:37:57 +0200

    chore: delete old services

--- services/profile/src/types/refinedev-core.d.ts
+++
@@ -1,9 +0,0 @@
-import "@refinedev/core";
-
-declare module "@refinedev/core" {
-  import { Login, QueryLoginArgs } from "../__generated__/codegen";
-
-  export interface DataProvider {
-    login: (args: QueryLoginArgs) => Promise<Login>;
-  }
-}
