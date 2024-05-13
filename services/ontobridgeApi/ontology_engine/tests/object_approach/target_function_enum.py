e7c0dcc3d53f80d24580961ad94741dec123b8d0
Yannick Le Razer <ylerazer@hotmail.com>
2024-05-13 09:45:37 +0200

    add ontology engine structure

---
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/target_function_enum.py
@@ -0,0 +1,12 @@
+import enum
+
+class TargetFunctionEnum(enum):
+    none = 0
+    generateId = 1
+    asIs_WithLang = 2
+    date__to__xsd = 3
+    as_is = 4
+    search__for__mapping__with__source = 5
+
+    def __str__(self):
+            return f'fno:{self.name.replace('__', '-')}'
