2f518e9778fc770389f8ffd9d8f129dcea503ee1
Yannick Le Razer <ylerazer@hotmail.com>
2024-05-13 18:08:40 +0200

    add first test.

---
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/profile.py
@@ -0,0 +1,15 @@
+class Profile:
+    def __init__(self, id : str = '',email : str = '',type : str = '' ) -> None:
+        self.id : str = id
+        self.email : str = email
+        self.type : str = type
+    
+    def toJsonLD(self) -> dict:
+        jsonLD = {}
+        for attr, value in self.__dict__.items():
+            if value == '':
+                continue
+            if value == None:
+                continue
+            jsonLD[attr] = value
+        return jsonLD
---
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/tools.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing import List
+from ontology_engine.tests.object_approach.pref_label import PrefLabel
+
+
+def toJsonLD(object) -> dict:
+        jsonLD = {}
+        for attr, value in object.__dict__.items():
+            if value == '':
+                continue
+            if value == None:
+                continue
+            if isinstance(value, List):
+                if len(value) == 0:
+                    continue
+            if isinstance(value, PrefLabel):
+                jsonLD[attr] = {}
+                jsonLD[attr]['@value'] = value.value
+                jsonLD[attr]['@language'] = value.language
+                continue
+            if isinstance(value, datetime):
+                jsonLD[attr] = value.strftime("%Y-%m-%d")
+                continue
+            jsonLD[attr] = value
+        return jsonLD
+    
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
