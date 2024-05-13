6ce0c8da31208d04624da340713888efbd7947c4
Yannick Le Razer <ylerazer@hotmail.com>
2024-05-13 18:08:40 +0200

    add first test.

--- services/ontobridgeApi/ontology_engine/tests/object_approach/rule.py
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/rule.py
@@ -1,19 +1,28 @@
-
-from ontology_engine.tests.object_approach.target_function_enum import TargetFunctionEnum
-
-
-class Skill:
-    def __init__(self) -> None:
-        self.id : str = ''
-        self.sourcePath : str = ''
-        self.sourcePath : str = ''
-        self.targetClass : str = ''
-        self.targetProperty : str = ''
-        self.targetFunction : TargetFunctionEnum = ''
-        self.generateId : bool = ''
-        self.relationTo : str = ''
-        self.relationName : str = ''
-        self.relationNameInverse : str = ''
+class Rule:
+    def __init__(self, id : str = '', sourcePath : str = '', targetClass : str = '',targetLang : str = '', targetProperty : str = '', targetFunction : str = '', generateId : bool = False, relationTo : str = '', relationName : str = '',relationNameInverse : str = '') -> None:
+        self.id :str = id
+        self.sourcePath : str = sourcePath
+        self.targetClass : str = targetClass
+        self.targetLang : str = targetLang
+        self.targetProperty : str = targetProperty
+        self.targetFunction : str = targetFunction
+        self.generateId : bool = True if generateId == "true" else False
+        self.relationTo : str = relationTo
+        self.relationName : str = relationName
+        self.relationNameInverse : str = relationNameInverse
         
         
-        
+    def __str__(self):
+        return f"""
+            id : {self.id}
+            sourcePath : {self.sourcePath}
+            targetClass : {self.targetClass}
+            targetLang : {self.targetLang}
+            targetProperty : {self.targetProperty}
+            targetFunction : {self.targetFunction}
+            generateId : {self.generateId}
+            relationTo : {self.relationTo}
+            relationName : {self.relationName}
+            relationNameInverse : {self.relationNameInverse}
+        """
+   
--- services/ontobridgeApi/ontology_engine/tests/object_approach/skill.py
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/skill.py
@@ -1,8 +1,19 @@
+from typing import List
+
 class Skill:
-    def __init__(self) -> None:
-        self.experience : str = ''
-        self.category : str = ''
-        self.skillLevelScale : str = ''
-        self.skillLevelValue : str = ''
-        
-        
+    def __init__(self, experience : str = '', category : str = '', skillLevelScale : List[str] = [], skillLevelValue : str = '', type : str = '') -> None:
+        self.experience : str = experience
+        self.category : str = category
+        self.skillLevelScale : List[str] = skillLevelScale
+        self.skillLevelValue : str = skillLevelValue
+        self.type : str = type
+    
+    def toJsonLD(self) -> dict:
+        jsonLD = {}
+        for attr, value in self.__dict__.items():
+            if value == '':
+                continue
+            if value == None:
+                continue
+            jsonLD[attr] = value
+        return jsonLD
