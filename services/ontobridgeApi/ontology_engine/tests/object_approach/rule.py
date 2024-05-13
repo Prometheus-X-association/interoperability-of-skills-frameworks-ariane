802174116a9c5c9c83ef35bf473ded01bd4b96d5
Yannick Le Razer <ylerazer@hotmail.com>
2024-05-13 09:45:37 +0200

    add ontology engine structure

---
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/rule.py
@@ -0,0 +1,19 @@
+
+from ontology_engine.tests.object_approach.target_function_enum import TargetFunctionEnum
+
+
+class Skill:
+    def __init__(self) -> None:
+        self.id : str = ''
+        self.sourcePath : str = ''
+        self.sourcePath : str = ''
+        self.targetClass : str = ''
+        self.targetProperty : str = ''
+        self.targetFunction : TargetFunctionEnum = ''
+        self.generateId : bool = ''
+        self.relationTo : str = ''
+        self.relationName : str = ''
+        self.relationNameInverse : str = ''
+        
+        
+        
