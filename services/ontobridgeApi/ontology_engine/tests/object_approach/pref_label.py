56aaec63377beeeb0bbeb6b9338a6e307a1c8f75
Hugo Victor <hugo.victor@cotral.com>
2024-05-20 22:58:58 +0200

    hello service

--- services/ontobridgeApi/ontology_engine/tests/object_approach/pref_label.py
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/pref_label.py
@@ -1,4 +1,4 @@
 class PrefLabel:
-    def __init__(self, value : str = '', language : str = '') -> None:
-        self.value : str = value
-        self.language : str = language
+    def __init__(self, value: str = "", language: str = "") -> None:
+        self.value: str = value
+        self.language: str = language
