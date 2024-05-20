49713b5175f043eccc0c8baed51967cfbbcd9ac6
Hugo Victor <hugo.victor@cotral.com>
2024-05-20 22:58:58 +0200

    hello service

--- services/ontobridgeApi/ontology_engine/tests/object_approach/skill.py
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/skill.py
@@ -1,17 +1,25 @@
 from typing import List
 
+
 class Skill:
-    def __init__(self, experience : str = '', category : str = '', skillLevelScale : List[str] = [], skillLevelValue : str = '', type : str = '') -> None:
-        self.experience : str = experience
-        self.category : str = category
-        self.skillLevelScale : List[str] = skillLevelScale
-        self.skillLevelValue : str = skillLevelValue
-        self.type : str = type
-    
+    def __init__(
+        self,
+        experience: str = "",
+        category: str = "",
+        skillLevelScale: List[str] = [],
+        skillLevelValue: str = "",
+        type: str = "",
+    ) -> None:
+        self.experience: str = experience
+        self.category: str = category
+        self.skillLevelScale: List[str] = skillLevelScale
+        self.skillLevelValue: str = skillLevelValue
+        self.type: str = type
+
     def toJsonLD(self) -> dict:
         jsonLD = {}
         for attr, value in self.__dict__.items():
-            if value == '':
+            if value == "":
                 continue
             if value == None:
                 continue
