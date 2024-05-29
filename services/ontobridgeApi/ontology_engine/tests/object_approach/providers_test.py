354c9deec8dd1817f1dfeb087b3bd420664f41a5
Yannick Le Razer <ylerazer@hotmail.com>
2024-05-29 09:45:41 +0200

    change line length in black 88 -> 128

--- services/ontobridgeApi/ontology_engine/tests/object_approach/providers_test.py
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/providers_test.py
@@ -51,9 +51,7 @@ def test_apply_rules_gamingtest():
 
     json_result = json.dumps(serialisation, sort_keys=True)
 
-    expected_data = get_tests(
-        f"{providerName}-minimal-structure.output.jsonld", providerName
-    )
+    expected_data = get_tests(f"{providerName}-minimal-structure.output.jsonld", providerName)
     expected_output = json.dumps(expected_data, sort_keys=True)
     print("--------------------------------------")
     print(ordered(json_result))
@@ -83,9 +81,7 @@ def test_apply_tree_rules_gamingtest():
 
     json_result = json.dumps(serialisation, sort_keys=True)
 
-    expected_data = get_tests(
-        f"{providerName}-minimal-structure.output.jsonld", providerName
-    )
+    expected_data = get_tests(f"{providerName}-minimal-structure.output.jsonld", providerName)
     expected_output = json.dumps(expected_data, sort_keys=True)
     print("--------------------------------------")
     print(ordered(json_result))
@@ -115,9 +111,7 @@ def test_apply_tree_rules_gamingtest_all():
 
     json_result = json.dumps(serialisation, sort_keys=True)
 
-    expected_data = get_tests(
-        f"{providerName}-all-structure.output.jsonld", providerName
-    )
+    expected_data = get_tests(f"{providerName}-all-structure.output.jsonld", providerName)
     expected_output = json.dumps(expected_data, sort_keys=True)
     print("--------------------------------------")
     print(ordered(json_result))
