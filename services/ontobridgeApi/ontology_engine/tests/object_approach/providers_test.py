a136b83459a1b7258756f8fea2cefe5f6fe42a35
Yannick Le Razer <ylerazer@hotmail.com>
2024-05-29 12:16:48 +0200

    modify interim rules implement targetValue

--- services/ontobridgeApi/ontology_engine/tests/object_approach/providers_test.py
+++ services/ontobridgeApi/ontology_engine/tests/object_approach/providers_test.py
@@ -194,7 +194,7 @@ def test_apply_tree_rules_interim():
     minimal_output = get_tests(f"{providerName}-minimal.json", providerName)
 
     ruleEngine = RuleEngine(rules)
-
+    
     serialisation = ruleEngine.generate(minimal_output, by_tree=True)
 
     json_result = json.dumps(serialisation, sort_keys=True)
