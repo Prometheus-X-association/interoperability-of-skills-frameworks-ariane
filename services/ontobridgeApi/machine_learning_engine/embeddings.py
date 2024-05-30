b7d958ac6d2801f8570b997f9987df6679567a87
Hugo Victor <hugo.victor@cotral.com>
2024-05-30 23:14:16 +0200

    machine learning local service

--- services/ontobridgeApi/machine_learning_engine/embeddings.py
+++ services/ontobridgeApi/machine_learning_engine/embeddings.py
@@ -1,12 +1,16 @@
 from sentence_transformers import SentenceTransformer
 import torch
+<<<<<<< HEAD
 import os
+=======
+>>>>>>> e23b4cb (machine learning local service)
 
 
 class Embeddings:
 
     def __init__(self, model_identifier: str) -> None:
         # Load the model
+<<<<<<< HEAD
         self.model_identifier = model_identifier        
         cwd=os.getcwd()
         self.modelPath =  f"{cwd}/machine_learning_engine/models_dump/{model_identifier}"
@@ -18,10 +22,20 @@ class Embeddings:
         else:
             self.model = SentenceTransformer(self.modelPath)            
 
+=======
+        self.model_identifier = model_identifier
+        self.model = SentenceTransformer(model_identifier)
+>>>>>>> e23b4cb (machine learning local service)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model.to(self.device)
 
     def embed_texts(self, sentences: list[list[str]]) -> list[list[str]]:
+<<<<<<< HEAD
         embeddings = self.model.encode(sentences, convert_to_tensor=True, device=self.device)
+=======
+        embeddings = self.model.encode(
+            sentences, convert_to_tensor=True, device=self.device
+        )
+>>>>>>> e23b4cb (machine learning local service)
         embeddings_vector = embeddings.cpu().numpy().tolist()
         return embeddings_vector
