e8aba3d660a9c68a2d0438d1173767df187355e1
Hugo Victor <hugo.victor@cotral.com>
2024-06-07 09:25:27 +0200

    term matching service

--- services/ontobridgeApi/api/routers/__init__.py
+++ services/ontobridgeApi/api/routers/__init__.py
@@ -3,9 +3,12 @@ from .ontology import *
 <<<<<<< HEAD
 from .machine_learning import *
 from .term_matching import *
+<<<<<<< HEAD
 from .onto_bridge import *
 =======
 >>>>>>> 2abe167 (fast api)
 =======
 from .machine_learning import *
 >>>>>>> 8044f9d (embedding endpoint)
+=======
+>>>>>>> 81c1026 (term matching service)
