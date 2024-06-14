0841aa0235f97c77be465cdf70012ac122c5f3c8
Hugo Victor <hugo.victor@cotral.com>
2024-06-14 11:10:37 +0200

    dynamic ml model

--- services/ontobridgeApi/machine_learning_engine/embeddings.py
+++ services/ontobridgeApi/machine_learning_engine/embeddings.py
@@ -1,9 +1,13 @@
 from sentence_transformers import SentenceTransformer
 import torch
 <<<<<<< HEAD
+<<<<<<< HEAD
 import os
 =======
 >>>>>>> e23b4cb (machine learning local service)
+=======
+import os
+>>>>>>> 29f9ce5 (dynamic ml model)
 
 
 class Embeddings:
@@ -11,21 +15,30 @@ class Embeddings:
     def __init__(self, model_identifier: str) -> None:
         # Load the model
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 29f9ce5 (dynamic ml model)
         self.model_identifier = model_identifier        
         cwd=os.getcwd()
         self.modelPath =  f"{cwd}/machine_learning_engine/models_dump/{model_identifier}"
 
         if not os.path.exists(self.modelPath):
+<<<<<<< HEAD
             print("Download the model")
+=======
+>>>>>>> 29f9ce5 (dynamic ml model)
             self.model = SentenceTransformer(model_identifier)
             self.model.save(self.modelPath)
         else:
             self.model = SentenceTransformer(self.modelPath)            
 
+<<<<<<< HEAD
 =======
         self.model_identifier = model_identifier
         self.model = SentenceTransformer(model_identifier)
 >>>>>>> e23b4cb (machine learning local service)
+=======
+>>>>>>> 29f9ce5 (dynamic ml model)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model.to(self.device)
 
