5a630e47e3ad0d48474e8eac64e6303736de0a0a
Yannick Le Razer <ylerazer@hotmail.com>
2024-06-21 14:17:36 +0200

    fix term_matching_service.py

--- services/ontobridgeApi/api/services/term_matching_service/term_matching_service.py
+++ services/ontobridgeApi/api/services/term_matching_service/term_matching_service.py
@@ -17,10 +17,10 @@ class TermMatchingService:
     def get_gql_term_create_or_find(self, rdf):
         for instance in rdf['graph']:
             if '__term__' in instance:
-                concept_pref_label = instance["__term__"]['value']
-                collection_pref_label = instance["__term__"]['scale']
-                collection_category = instance["__term__"]['collection_category']
-                provider_name =  instance["__term__"]['provider']
+                concept_pref_label = instance["__term__"]['value'] # 0.8
+                collection_pref_label = instance["__term__"]['scale'] #skill 
+                collection_category = instance["__term__"]['collection_category'] #
+                provider_name =  instance["__term__"]['provider'] # provider 
                 concept = self.term_matching_engine.get_gql_create_or_find_term(provider_name, collection_pref_label,collection_category, concept_pref_label)
         return rdf
         
