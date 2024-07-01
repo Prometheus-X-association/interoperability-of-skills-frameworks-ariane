f5f94683f9aac3c2a317408138fe742a533f9d78
Yannick Le Razer <ylerazer@hotmail.com>
2024-07-01 10:05:10 +0200

    add transformation still missing endpoint.

--- services/ontobridgeApi/api/main.py
+++ services/ontobridgeApi/api/main.py
@@ -20,6 +20,7 @@ def get_app():
     app.include_router(routers.ontology.router, prefix="/ontologies")
     app.include_router(routers.machine_learning.router, prefix="/machine_learning")
     app.include_router(routers.term_matching.router, prefix="/term_matchings")
+    app.include_router(routers.router, prefix="/onto_bridge")
     return app
 
 
