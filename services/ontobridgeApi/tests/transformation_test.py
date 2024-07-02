56d51ac92a24431cea477237316f023e1c099353
Yannick Le Razer <ylerazer@hotmail.com>
2024-07-02 17:18:05 +0200

    fix scale for skill level fix gaming test full generation

--- services/ontobridgeApi/tests/transformation_test.py
+++ services/ontobridgeApi/tests/transformation_test.py
@@ -206,6 +206,42 @@ def test_apply_tree_rules_pitangoo():
         f"{providerName}_full_expected_data",
     )
 
+def test_apply_tree_rules_gamingtest():
+    providerName = "gamingtest"
+
+    rules = get_rules(providerName)
+    minimal_output = get_tests(f"{providerName}-minimal.json", providerName)
+
+    ruleEngine = RuleEngine(rules, providerName)
+
+    serialisation = ruleEngine.generate(minimal_output, by_tree=True)
+    
+    term_matching_engine = TermMatchingEngine()
+    serialisationWithTerm = term_matching_engine.generate(serialisation, providerName)
+        
+    target_framework = 'rome'
+    
+    matching_source_engine = SourceMappingEngine()
+    serialisationWithTermAndMatching = matching_source_engine.generate(serialisationWithTerm, target_framework,  providerName)
+
+    json_result = json.dumps(serialisationWithTermAndMatching, sort_keys=True)
+
+    expected_data = get_tests(f"{providerName}-minimal.output.jsonld", providerName)
+    expected_output = json.dumps(expected_data, sort_keys=True)
+    print("--------------------------------------")
+    print(ordered(json_result))
+    print("VS")
+    print(ordered(expected_output))
+    print("--------------------------------------")
+    write_result(
+        json.dumps(serialisation, sort_keys=True, indent=1),
+        f"{providerName}_full_generated_data",
+    )
+    write_result(
+        json.dumps(expected_data, sort_keys=True, indent=1),
+        f"{providerName}_full_expected_data",
+    )
+
 
 def test_apply_tree_rules_gamingtest():
     providerName = "gamingtest"
