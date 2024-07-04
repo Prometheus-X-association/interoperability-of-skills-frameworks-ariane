fdb22646e962a46c72b5110d26f9ee288a334791
Yannick Le Razer <ylerazer@hotmail.com>
2024-07-04 22:29:15 +0200

    refactor and update folders

--- services/ontobridgeApi/api/services/machine_learning_service/maching_learning_service.py
+++
@@ -1,7 +0,0 @@
-from term_matching_engine.source_mapping_engine import SourceMappingEngine
-
-
-class SourceMappingService:
-
-    def __init__(self) -> None:
-        self.source_mapping_engine = SourceMappingEngine()
